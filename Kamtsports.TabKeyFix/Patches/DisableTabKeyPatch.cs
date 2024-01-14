4988cb0eaa6f8204db074744ae01696873d0fe88
Marcus Kauffeld <tamino202@gmail.com>
2024-01-14 20:57:29 +0100

    [MK]added plugin code

---
+++ Kamtsports.TabKeyFix/Patches/DisableTabKeyPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace Kamtsports.TabKeyFix.Patches
+{
+    [HarmonyPatch]
+    internal static class DisableTabKeyPatch
+    {
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Terminal), "PressESC")]
+        public static bool DisableTabKey(ref InputAction.CallbackContext context)
+        {
+            var name = context.control.name;
+            return !name.Equals("tab");
+        }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HUDManager), nameof(HUDManager.ChangeControlTip))]
+        public static void ChangeHintToEsc(ref string changeTo)
+        {
+            if (changeTo.Equals("Quit terminal : [TAB]"))
+            {
+                changeTo = "Quit terminal : [ESC]";
+            }
+        }
+    }
+    
+}
---
+++ Kamtsports.TabKeyFix/TabKeyFixPlugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Kamtsports.TabKeyFix;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class TabKeyFixPlugin: BaseUnityPlugin 
+{
+    
+    private readonly Harmony m_Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+    private void Awake()
+    {
+        Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} is loading...");
+
+        Logger.LogInfo($"Installing patches");
+        m_Harmony.PatchAll(typeof(TabKeyFixPlugin).Assembly);
+        
+
+        DontDestroyOnLoad(this);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
